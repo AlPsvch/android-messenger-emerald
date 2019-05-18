dca9d88255ae37e9ac1cce620ddf3995dbc57ce9
AlPsvch <pasevich.aleksej@gmail.com>
2019-05-18 18:18:10 +0300

    Andriod messenger Emerald created

---
+++ app/src/main/java/com/example/emerald/RegisterActivity.java
@@ -0,0 +1,71 @@
+package com.example.emerald;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputLayout mDisplayName;
+    private TextInputLayout mEmail;
+    private TextInputLayout mPassword;
+    private Button mCreateButton;
+
+    //Firebase Auth
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        //Registration fields
+        mDisplayName = findViewById(R.id.reg_display_name);
+        mEmail = findViewById(R.id.reg_email);
+        mPassword = findViewById(R.id.reg_password);
+        mCreateButton = findViewById(R.id.reg_create_btn);
+
+        mCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String displayName = mDisplayName.getEditText().getText().toString();
+                    String email = mEmail.getEditText().getText().toString();
+                    String password = mPassword.getEditText().getText().toString();
+
+                    registerUser(displayName, email, password);
+                } catch(NullPointerException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        });
+    }
+
+    private void registerUser(String displayName, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "You got some error", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/emerald/StartActivity.java
@@ -0,0 +1,28 @@
+package com.example.emerald;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button mRegBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        mRegBtn = findViewById(R.id.start_reg_button);
+
+        mRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regIntent = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(regIntent);
+            }
+        });
+    }
+}
